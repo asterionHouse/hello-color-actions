af1cfe4b95dcb2b14b25b19fed24ffa08b11460e
JonatanMR <jonatan.m.rangel@gmail.com>
2023-10-27 21:14:10 -0600

    cambio en mensaje a mostrar

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hola Mundo CI CD (Jonatan Mendez Rangel)";
 console.log(hello());
 exports.hello = hello;
